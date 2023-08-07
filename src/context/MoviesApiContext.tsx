0283f424e86add1099d21d1ff930a36315564de3
K-Othman <karim_othman88@hotmail.com>
2023-08-07 21:34:39 +0100

    creating the context api

---
+++ src/context/MoviesApiContext.tsx
@@ -0,0 +1,51 @@
+import { FC, ReactNode, createContext, useMemo, useState } from "react";
+
+import movieData from "../data/film.json";
+
+type Props = {
+  children: ReactNode;
+};
+
+export interface MovieData {
+  id: number;
+  Title: string;
+  Year: string;
+  Released: string;
+  Genre: string;
+  Actors: string;
+  Plot: string;
+  Poster: string;
+  Type: string;
+  Images: string[];
+}
+
+export interface IMoviesContext {
+  movieData: MovieData[];
+  movies: MovieData[];
+}
+
+export const MoviesContext = createContext<IMoviesContext>(
+  {} as IMoviesContext
+);
+
+const MoviesApiContext: FC<Props> = ({ children }) => {
+  const [movies, setMovies] = useState<MovieData[]>(movieData);
+
+  const MoviesContextValue = useMemo(
+    () => ({
+      movieData,
+      movies,
+      setMovies,
+    }),
+    [movies]
+  );
+  console.log(movies);
+
+  return (
+    <MoviesContext.Provider value={MoviesContextValue}>
+      {children}
+    </MoviesContext.Provider>
+  );
+};
+
+export default MoviesApiContext;
---
+++ src/pages/mainPage/MainPage.tsx
@@ -0,0 +1,5 @@
+const MainPage = () => {
+  return <div className="text-white">MainPage</div>;
+};
+
+export default MainPage;
