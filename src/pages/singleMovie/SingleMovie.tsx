dc2c8145c7c33b12f7fc8025f81cff9e553aef9c
K-Othman <karim_othman88@hotmail.com>
2023-08-14 11:46:35 +0100

    creating the single movie page

--- src/pages/singleMovie/SingleMovie.tsx
+++ src/pages/singleMovie/SingleMovie.tsx
@@ -1,5 +1,45 @@
+import { useContext } from "react";
+import { MoviesContext } from "../../context/MoviesApiContext";
+import { useParams } from "react-router-dom";
+
+interface SingleMovie {
+  movieId: number;
+}
+
 const SingleMovie = () => {
-  return <div>SingleMovie</div>;
+  const { allMovies } = useContext(MoviesContext);
+  const { movieId } = useParams<SingleMovie>();
+
+  const filteredMovies = allMovies.filter(
+    (movie) => movie.id === parseInt(movieId)
+  );
+  console.log(filteredMovies);
+
+  return (
+    <section>
+      {filteredMovies.length > 0 && (
+        <article>
+          {filteredMovies.map((movie) => {
+            return (
+              <div key={movie.id}>
+                <ul>
+                  <li>{movie.Title}</li>
+                  <li>{movie.Genre}</li>
+                  <li>{movie.Rate}</li>
+
+                  <li>
+                    {" "}
+                    <img src={movie.Poster} alt={movie.Genre} />
+                  </li>
+                  <li>{movie.Released}</li>
+                </ul>
+              </div>
+            );
+          })}
+        </article>
+      )}
+    </section>
+  );
 };
 
 export default SingleMovie;
